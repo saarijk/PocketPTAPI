8f58eb563f1c8a91725a0c46302768ceb6b8c6c2
jsaari <jsaari@student.oulu.fi>
2022-12-04 12:19:09 +0200

    Initial Project and Database (in memory)
    can get/post users

---
+++ TestingApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TestingApi.Models
+{
+    public class User { 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public int HeightCm { get; set; }
+        public double Bmi { get; set; }
+        //public double Bmi => (Weight / ((HeightCm/100) * (HeightCm/100)));
+        public long Id { get; set; }
+
+    }
+}
---
+++ TestingApi/Models/UserContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TestingApi.Models
+{
+    public class UserContext : DbContext 
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
